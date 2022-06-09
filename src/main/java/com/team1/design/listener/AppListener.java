80dacadf7bd2cacda536bf5b78e7dd3661cae1b1
ttruu <rhdtlr300@naver.com>
2022-06-09 16:42:01 +0900

    Initial commit

---
+++ src/main/java/com/team1/design/listener/AppListener.java
@@ -0,0 +1,47 @@
+package com.team1.design.listener;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+import javax.servlet.annotation.WebListener;
+
+/**
+ * Application Lifecycle Listener implementation class AppListener
+ *
+ */
+
+public class AppListener implements ServletContextListener {
+
+    /**
+     * Default constructor. 
+     */
+    public AppListener() {
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+     * @see ServletContextListener#contextDestroyed(ServletContextEvent)
+     */
+    public void contextDestroyed(ServletContextEvent sce)  { 
+         // TODO Auto-generated method stub
+    }
+
+	/**
+     * @see ServletContextListener#contextInitialized(ServletContextEvent)
+     */
+    public void contextInitialized(ServletContextEvent sce)  { 
+         ServletContext application = sce.getServletContext();
+         
+         application.setAttribute("appRoot", application.getContextPath());
+         
+         
+			/* String imageUrl = "https://bucket0207-223344.s3.ap-northeast-2.amazonaws.com";
+			 * application.setAttribute("imageUrl", imageUrl);
+			 */
+    }
+	
+}
+
+
+
+
